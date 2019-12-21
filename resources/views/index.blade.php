fc09055d7fe21c5f94ade1f473d760b6f46d6208
Baillie O'Grady <hello@baillieogrady.com>
2019-12-21 14:02:40 +0000

    Add Lazy load for images

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -13,6 +13,7 @@
   @while (have_posts()) @php the_post() @endphp
     @include('partials.content-'.get_post_type())
   @endwhile
+  <img data-src="https://images.unsplash.com/photo-1576909855973-eb0bb6ab8986?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80" alt="" class="lozad">
 
   {!! get_the_posts_navigation() !!}
 @endsection
--- resources/views/page.blade.php
+++ resources/views/page.blade.php
@@ -3,6 +3,5 @@
 @section('content')
   @while(have_posts()) @php the_post() @endphp
     @include('partials.page-header')
-    @include('partials.content-page')
-  @endwhile
+    @include('partials.content-page')  @endwhile
 @endsection
