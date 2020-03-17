9f8ef2325f09e75237ff3357c69c74e2ac813078
Baillie O'Grady <hello@baillieogrady.com>
2020-03-17 13:56:40 +0000

    Comment out tailwind setup

--- resources/assets/styles/tailwind.js
+++ resources/assets/styles/tailwind.js
@@ -1,7 +1,7 @@
-module.exports = {
-  theme: {
-    extend: {},
-  },
-  variants: {},
-  plugins: [],
-}
+// module.exports = {
+//   theme: {
+//     extend: {},
+//   },
+//   variants: {},
+//   plugins: [],
+// }
