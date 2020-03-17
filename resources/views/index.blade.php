2131da501bdf61f3bd2432a77d26a3f2980bb21f
Baillie O'Grady <hello@baillieogrady.com>
2020-03-17 14:01:38 +0000

    Remove tailwind text markup

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,17 +2,7 @@
 
 @section('content')
   @include('partials.page-header')
-  <!-- Full width column -->
-  <div class="flex mb-4">
-    <div class="w-full bg-gray-500 h-12"></div>
-  </div>
-
-  <!-- Two columns -->
-<div class="flex mb-4">
-  <div class="w-1/2 bg-gray-400 h-12"></div>
-  <div class="w-1/2 bg-gray-500 h-12"></div>
-</div>
-
+  
   {{-- <div id="posts"></div> --}}
 {{--   
   @if (!have_posts())
